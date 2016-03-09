3cbe49a89b1e68ef6c27ce61421a1a24d0a45906
Daniel Egert <daniel.egert@wheregroup.com>
2016-03-09 16:06:13 +0100

    Add to SpatialiteShellDriver add column method

--- src/Eslider/SpatialiteShellDriver.php
+++ src/Eslider/SpatialiteShellDriver.php
@@ -368,4 +368,28 @@ class SpatialiteShellDriver
         }
         return $r;
     }
+
+    /**
+     * Add column by table name and column name
+     *
+     * Types:
+     *
+     * * NULL. The value is a NULL value.
+     * * INTEGER. The value is a signed integer, stored in 1, 2, 3, 4, 6, or 8 bytes depending on the magnitude of the value.
+     * * REAL. The value is a floating point value, stored as an 8-byte IEEE floating point number.
+     * * TEXT. The value is a text string, stored using the database encoding (UTF-8, UTF-16BE or UTF-16LE).
+     * * BLOB. The value is a blob of data, stored exactly as it was input.
+     *
+     * @see https://www.sqlite.org/datatype3.html
+     *
+     * @param string $tableName Table name
+     * @param string $columnName Column name
+     * @param string $type Type as string (https://www.sqlite.org/datatype3.html)
+     *
+     * @return array
+     */
+    public function addColumn($tableName, $columnName, $type="TEXT")
+    {
+        return $this->query("ALTER TABLE ". $this->quote($tableName)." ADD COLUMN " .$this->quote($columnName));
+    }
 }
